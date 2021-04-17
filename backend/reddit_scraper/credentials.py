9b5d4a04845a4d391e6a8e7ec280f1729fb06d22
Gabe <gabestangel70@gmail.com>
2021-04-17 12:59:58 -0500

    this is scraping now

---
+++ backend/reddit_scraper/credentials.py
@@ -0,0 +1,3 @@
+client_id = "1Nk8-jyeaLAv4w"
+client_secret = "VIK25f6Tsuqg_nc6pU4jClN6b_Y"
+user_agent = "idriveawhitecamry"
--- backend/reddit_scraper/scraper.py
+++ backend/reddit_scraper/scraper.py
@@ -2,10 +2,12 @@ import praw
 from textblob import TextBlob
 import nltk
 import credentials # import the credential 
+import re
+import pickle
 
-# Download the VADAR tool and access it through the NLTK library.
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer 
+# # Download the VADAR tool and access it through the NLTK library.
+# nltk.download('vader_lexicon')
+# from nltk.sentiment.vader import SentimentIntensityAnalyzer 
 
 
 reddit = praw.Reddit(client_id=credentials.client_id,
@@ -15,11 +17,15 @@ reddit = praw.Reddit(client_id=credentials.client_id,
 
 
 
-# class ticker():
-#     def __init__(self,ticker)
+class Ticker(): ## ticker class, the dict is such that the key is a ticker and the value is a dict
+    def __init__(self,ticker,full_name,sector):
+        self.ticker = ticker # ticker ex: TSLA
+        self.full_name = full_name # full name of the company
+        self.sector = sector # sector of the ticker
+        self.comments = [] # list of comment objects provided by PRAW
 
 # get tickers from the csv
-def get_tickers(file_name):
+def get_tickers(file_name) -> dict:
     tickers = {} # dict where the ticker is the key and the value is a tuple of (full)
     file = open(file_name)
     lines = file.readlines()
@@ -28,24 +34,36 @@ def get_tickers(file_name):
         ticker = split_line[0]
         company = split_line[1]
         sector = split_line[2]
-        tickers[split_line[0]] = 0
-        print("Ticker: " + ticker + " Full name: " + company + "sector: " + sector)
+        tickers[ticker] = Ticker(ticker, company, sector)
+    return tickers
 
-def get_posts(target_subreddit, post_limit, time_frame) -> dict:
-    tickers = {} #dict where the ticker is the key and the comments are the values
+def get_posts(target_subreddit, post_limit) -> dict:
+    tickers = get_tickers("tickers.txt") # get the ticker dict
     posts = reddit.subreddit(target_subreddit).new(limit=post_limit)
+    count = 0
     for post in posts:   #iterate through posts
-        try:
-            post.comments.replace_more(limit=None)
-            for comment in post.comments.list():
-                print(comment.body)
-        except:
-            contine
+        if "GME" not in post.title:
+            count += 1
+            print(count)
+            try:
+                post.comments.replace_more(limit=None) # this generates a tree of comments that is iteratable
+                for comment in post.comments.list():
+                    for word in str(comment.body).split(" "): #iterate through each word in the comment, .split() creates an array of words instead of iterating through charecters
+                        word = re.sub(r'\W+','', word) #remove charecters that we dont care about
+                        if word in tickers.keys(): # see if the currrent word is a valid ticker
+                            tickers[word].comments.append(comment) # if so, append the comment to the object stored at tickers[word]
+                            
+            except Exception as E: #ghetto error handling
+                print(E)
+                continue
+    #save the dict locally for further processing
+    pkl_file = open("tickers.pkl", "wb")
+    pickle.dump(tickers, pkl_file)
 
-def evaulate_comment(comment) -> int: #evaluates the comment, returns an integer from -1 to +1 indicating it's signifigance
 
 
 
 if __name__ == "__main__":
-    get_posts("wallstreetbets", 100, "week")
+    #tickers = get_tickers("tickers.txt")
+    get_posts("investing", 1000)
     #get_tickers("tickers.txt")
