d189f63db70b81d285ed0ebbb763c28228d679f6
Jakob Lybarger <jakoblybarger911@gmail.com>
2021-04-17 21:28:26 -0500

    Stub chart added

--- apollo/src/App.js
+++ apollo/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+// import NavBar from './components/Navbar';
+import StockChart from './components/StockChart';
 
 class App extends Component {
   render() {
     return (
-      <div><h1>Apollo</h1></div>
+      <div>
+        hello
+        <StockChart />
+      </div>
     );
   }
 };
