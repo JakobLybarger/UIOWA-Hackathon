8caa8630b25076a9a5a1418dcd7e09637d3d719e
Jakob Lybarger <jakoblybarger911@gmail.com>
2021-04-18 12:38:25 -0500

    Final update unfortunately

--- apollo/src/components/StockChart.js
+++ apollo/src/components/StockChart.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box'
 import { Line } from "react-chartjs-2";
 import './StockChart.css';
 
 class StockChart extends Component {
-    render() {
-        const data = {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-            datasets: [
-                {
-                    label: "First dataset",
-                    data: [33, 53, 85, 41, 44, 65, 34, 54, 78, 67, 365, 56],
-                    fill: true,
-                    backgroundColor: "rgba(242, 167, 39, 0.2)",
-                    borderColor: "#F2A737"
-                }
-            ]
-        };
-        
+    render() {   
         return (
-        <Paper className="container">
-            <Line data={data} />
-        </Paper>
+        <Box className="container">
+            <Line data={this.props.data} />
+        </Box>
         );
     }
 };
